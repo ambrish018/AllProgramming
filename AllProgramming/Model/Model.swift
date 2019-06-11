bf43f93e1926426db56e3b6185df6012e0538bfb
ambrish018 <incredible.bil@gmail.com>
2019-06-12 00:14:56 +0530

    hi tutorial

---
+++ AllProgramming/Model/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  AllProgramming
+//
+//  Created by YASH COMPUTERS on 20/05/19.
+//  Copyright © 2019 YASH COMPUTERS. All rights reserved.
+//
+
+import Foundation
+
+struct MainModel {
+    let title : String
+}
--- AllProgramming/ViewController.swift
+++ AllProgramming/ViewController.swift
@@ -11,20 +11,28 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var myTableView: UITableView!
+    
+    var arr = [MainModel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
+        arr.append(MainModel(title:"CoreData"));
     }
 
 
 }
 extension ViewController : UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return arr.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Core Data"
+        cell.textLabel?.text = (arr[indexPath.row]).title
         return cell
         
     }
